1fae597bc9aa8d0e016ee47c28e03a443cf52d02
Marlon Monzon <martianworm17@gmail.com>
2019-08-30 17:31:06 +0800

    Save

--- mm-couchbase-spring-boot/src/main/java/com/example/demo/MmCouchbaseSpringBootApplication.java
+++ mm-couchbase-spring-boot/src/main/java/com/example/demo/MmCouchbaseSpringBootApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MmCouchbaseSpringBootApplication {
+public class MmCouchbaseSpringBootApplication implements CommandLineRunner {
+	
+	public static final String BUILDING_1 = "building::1";
+    public static final String COMPANY_2 = "company::2";
+	
+    @Autowired
+    private BuildingService buildingService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MmCouchbaseSpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Building building = new Building(BUILDING_1, "couchbase",
+                COMPANY_2, new ArrayList<>(), new ArrayList<>());
+        buildingService.save(building);
+		
 	}
 
 }
