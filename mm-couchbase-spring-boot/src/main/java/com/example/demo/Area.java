52fed1d5c4a710f3260d9328d496e1991e8e842b
Marlon Monzon <martianworm17@gmail.com>
2019-08-30 15:33:29 +0800

    Annotate Area class

--- mm-couchbase-spring-boot/src/main/java/com/example/demo/Area.java
+++ mm-couchbase-spring-boot/src/main/java/com/example/demo/Area.java
@@ -3,6 +3,16 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Area {
 	
 	private String id;
