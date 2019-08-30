43edf6c039b030c767c7a4063a8ce3728866583a
Marlon Monzon <martianworm17@gmail.com>
2019-08-30 14:26:49 +0800

    Initial commit

---
+++ mm-couchbase-spring-boot/src/main/java/com/example/demo/Building.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import java.awt.geom.Area;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.couchbase.core.mapping.Document;
+
+import com.couchbase.client.java.repository.annotation.Field;
+import com.couchbase.client.java.repository.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Building {
+	
+	@NotNull
+	@Id
+	private String id;
+	
+	@NotNull
+	@Id
+	private String name;
+	
+	@NotNull
+	@Field
+	private String companyId;
+	
+	private List<Area> areas = new ArrayList<>();
+	
+	private List<String> phoneNumbers = new ArrayList<>();
+
+}
---
+++ mm-couchbase-spring-boot/src/main/java/com/example/demo/MmCouchbaseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmCouchbaseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmCouchbaseSpringBootApplication.class, args);
+	}
+
+}
